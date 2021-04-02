c1d7c3d3d6bd97b20b74bb5e36e1d48233391b52
karolyim <matej.karolyi@gmail.com>
2021-04-02 15:56:56 +0200

    [MAINTENANCE] project init

---
+++ main.js
@@ -0,0 +1,45 @@
+let jmena = [
+    "Markéta",
+    "Kateřina",
+    "Júlia",
+    "Lucie",
+    "Aneta",
+    "Veronika",
+    "Petra",
+    "Olga",
+    "Naďa",
+    "Nikola",
+    "Eva",
+    "Anísa",
+    "Terezia",
+    "Diana",
+    "Anna",
+    "Iva",
+    "Denisa",
+    "Zuzana",
+    "Jana",
+    "Martina",
+    "Lenka",
+    "Iveta",
+    "Pavla",
+    "Lucie",
+    "Kamila",
+    "Soňa"
+];
+
+let tazenaJmena = [];
+
+function tahnoutJmeno() {
+    if (jmena.length === 0) {
+        return;
+    }
+
+    // Generujeme náhodný index
+
+    // Získáme výherní jméno na patřičném indexu
+
+    // Vyřadíme vylosované jméno z osudí
+
+    // Výherní jméno si uložíme do pole k ostatním výherním
+    
+}
