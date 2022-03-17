629e434abd375960214dcb43509051b10fed2c9f
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-17 08:19:08 +0100

    mainfunctionality

--- main.js
+++ main.js
@@ -35,11 +35,12 @@ function tahnoutJmeno() {
     }
 
     // Generujeme náhodný index
+    let vyherniIndex = Math.floor(Math.random() * jmena.length);
 
     // Získáme výherní jméno na patřičném indexu
-
+    let vyherniJmeno = jmena[vyherniIndex];
     // Vyřadíme vylosované jméno z osudí
-
+    jmena.splice(vyherniIndex, 1);
     // Výherní jméno si uložíme do pole k ostatním výherním
-    
+    tazenaJmena.push(vyherniJmeno);
 }
