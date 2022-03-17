87ea12b858de51f84f14e86540539b0a760c97cd
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-17 11:54:11 +0100

    add function zacnianovu

--- vyvolavac.js
+++ vyvolavac.js
@@ -28,6 +28,9 @@ let jmena = [
 ];
 
 let tazenaJmena = [];
+let seznamVyvolanych = document.querySelector('#seznam');
+let pocitadlo = document.querySelector('.pocitadlo');
+let vyherce = document.querySelector('#vyherka');
 
 function tahnoutJmeno() {
     if (jmena.length === 0) {
@@ -44,15 +47,13 @@ function tahnoutJmeno() {
     // Výherní jméno si uložíme do pole k ostatním výherním
     tazenaJmena.unshift(vyherniJmeno);
 
-    let vyherce = document.querySelector('#vyherka');
+
     vyherce.textContent = vyherniJmeno;
 
-    let seznamVyvolanych = document.querySelector('#seznam');
     seznamVyvolanych.textContent = tazenaJmena;
 
 
     function pocitadlo() {
-        let pocitadlo = document.querySelector('.pocitadlo');
         if (tazenaJmena.length === 1) {
             pocitadlo.textContent = "Jedna slečna už může v klidu odpočívat."
         } else if (tazenaJmena.length > 1 && tazenaJmena.length < 5) {
@@ -65,5 +66,34 @@ function tahnoutJmeno() {
 }
 
 function zacniZnovu() {
-
+    pocitadlo.textContent = "Nikdo se ještě nepokusil ohromit učitele";
+    seznamVyvolanych.textContent = "Seznam vyvolaných";
+    vyherka.textContent = "";
+    tazenaJmena.splice(0, tazenaJmena.length);
+    jmena.push("Markéta",
+        "Kateřina",
+        "Júlia",
+        "Lucie",
+        "Aneta",
+        "Veronika",
+        "Petra",
+        "Olga",
+        "Naďa",
+        "Nikola",
+        "Eva",
+        "Anísa",
+        "Terezia",
+        "Diana",
+        "Anna",
+        "Iva",
+        "Denisa",
+        "Zuzana",
+        "Jana",
+        "Martina",
+        "Lenka",
+        "Iveta",
+        "Pavla",
+        "Lucie",
+        "Kamila",
+        "Soňa")
 }
