4bb27d511bc8e8dbc9cc5252dfbc8aaeff094171
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-17 10:54:52 +0100

    add pocitadlo

--- vyvolavac.js
+++ vyvolavac.js
@@ -42,11 +42,24 @@ function tahnoutJmeno() {
     // Vyřadíme vylosované jméno z osudí
     jmena.splice(vyherniIndex, 1);
     // Výherní jméno si uložíme do pole k ostatním výherním
-    tazenaJmena.push(vyherniJmeno);
+    tazenaJmena.unshift(vyherniJmeno);
 
     let vyherce = document.querySelector('#vyherka');
     vyherce.textContent = vyherniJmeno;
 
     let seznamVyvolanych = document.querySelector('#seznam');
     seznamVyvolanych.textContent = tazenaJmena;
+
+
+    function pocitadlo() {
+        let pocitadlo = document.querySelector('.pocitadlo');
+        if (tazenaJmena.length === 1) {
+            pocitadlo.textContent = "Jedna slečna už může v klidu odpočívat."
+        } else if (tazenaJmena.length > 1 && tazenaJmena.length < 5) {
+            pocitadlo.textContent = tazenaJmena.length + " slečny jsou mimo nebezpečí"
+        } else if (tazenaJmena.length > 4)
+            pocitadlo.textContent = tazenaJmena.length + " slečen je mimo nebezpečí"
+    }
+    pocitadlo()
+
 }
