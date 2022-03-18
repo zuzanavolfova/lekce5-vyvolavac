3a2dbbc0a96cda481bc7b9ae9441978140d77acf
Zuzana Volfová <zuzka.volfova@gmail.com>
2022-03-18 08:26:20 +0100

    add function kopirujJmena, edit function zacniZnovu

--- vyvolavac.js
+++ vyvolavac.js
@@ -1,4 +1,4 @@
-let jmena = [
+let jmenaConst = [
     "Markéta",
     "Kateřina",
     "Júlia",
@@ -26,17 +26,20 @@ let jmena = [
     "Kamila",
     "Soňa"
 ];
+let jmena = [];
+kopirujJmena();
 
 let tazenaJmena = [];
 let seznamVyvolanych = document.querySelector('#seznam');
 let pocitadloText = document.querySelector('.pocitadlo');
 let vyherce = document.querySelector('#vyherka');
 
+
+
 function tahnoutJmeno() {
     if (jmena.length === 0) {
         return;
     }
-
     // Generujeme náhodný index
     let vyherniIndex = Math.floor(Math.random() * jmena.length);
 
@@ -47,20 +50,9 @@ function tahnoutJmeno() {
     // Výherní jméno si uložíme do pole k ostatním výherním
     tazenaJmena.unshift(vyherniJmeno);
 
-
     vyherce.textContent = vyherniJmeno;
 
     seznamVyvolanych.textContent = tazenaJmena;
-
-
-    function pocitadlo() {
-        if (tazenaJmena.length === 1) {
-            pocitadloText.textContent = "Jedna slečna už může v klidu odpočívat."
-        } else if (tazenaJmena.length > 1 && tazenaJmena.length < 5) {
-            pocitadloText.textContent = tazenaJmena.length + " slečny jsou mimo nebezpečí"
-        } else if (tazenaJmena.length > 4)
-            pocitadloText.textContent = tazenaJmena.length + " slečen je mimo nebezpečí"
-    }
 
     pocitadlo()
 }
@@ -70,5 +62,23 @@ function zacniZnovu() {
     seznamVyvolanych.textContent = "Seznam vyvolaných";
     vyherka.textContent = "";
     tazenaJmena.splice(0, tazenaJmena.length);
-    jmena.push("Markéta", "Kateřina", "Júlia", "Lucie", "Aneta", "Veronika", "Petra", "Olga", "Naďa", "Nikola", "Eva", "Anísa", "Terezia", "Diana", "Anna", "Iva", "Denisa", "Zuzana", "Jana", "Martina", "Lenka", "Iveta", "Pavla", "Lucie", "Kamila", "Soňa")
+    jmena.splice(0, jmena.length);
+    kopirujJmena();
+}
+
+function pocitadlo() {
+    if (tazenaJmena.length === 1) {
+        pocitadloText.textContent = "Jedna slečna už může v klidu odpočívat."
+    } else if (tazenaJmena.length > 1 && tazenaJmena.length < 5) {
+        pocitadloText.textContent = tazenaJmena.length + " slečny jsou mimo nebezpečí"
+    } else if (tazenaJmena.length > 4)
+        pocitadloText.textContent = tazenaJmena.length + " slečen je mimo nebezpečí"
+}
+
+function kopirujJmena() {
+    // for (let i = 0; i< jmenaConst.length; i++)
+    // {
+    //     jmena.push(kopirujJmena[i]);
+    // }
+    jmena.push(...jmenaConst);
 }
